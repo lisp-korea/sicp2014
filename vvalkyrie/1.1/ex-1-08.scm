33072ae9cb126b647750fbe3a6afd2bb4c355eb3
ikaprk <ikpark@gmail.com>
2014-11-24 15:59:50 +0900

    ex 1.3, 1.7, 18 by vvalkyrie

---
+++ vvalkyrie/1.1/ex-1-08.scm
@@ -0,0 +1,22 @@
+#lang planet neil/sicp
+
+(define (square x)
+  (* x x))
+
+(define (cube x)
+  (* x x x))
+
+(define (cbrt-iter guess x)
+  (if (good-enough? guess x)
+    guess
+    (cbrt-iter (improve guess x) x)))
+
+(define (improve guess x)
+  (/ (+ (/ x (square guess)) (* guess 2)) 3))
+
+(define (good-enough? guess x)
+  (< (abs (- (cube guess) x)) 0.001))
+
+(define (cbrt x)
+  (cbrt-iter 1.0 x))
+
