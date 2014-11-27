19cfbb2217cd9b458bb9e85df0a3b77f8da48169
Daniel Jun <unipro.kr@gmail.com>
2014-11-27 13:34:12 +0900

    ex 1.16 updated by unipro

--- unipro/1.2/ex-1.16.scm
+++ unipro/1.2/ex-1.16.scm
@@ -4,7 +4,7 @@
    (cond ((= nr 0) 1)
 	 ((= nr 1) (* a br))
 	 ((even? nr) (expr-recur a (square br) (/ nr 2)))
-	 (else (expr-recur (* a b) br (- nr 1)))))
+	 (else (expr-recur (* a br) br (- nr 1)))))
  (expr-recur 1 b n))
 
 (define (even? n)
