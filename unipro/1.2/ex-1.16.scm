d062a64f215335232aabb84ad07ff7e9c5b37719
Daniel Jun <unipro.kr@gmail.com>
2014-12-02 12:31:23 +0900

    ex 1.16 updated by unipro

--- unipro/1.2/ex-1.16.scm
+++ unipro/1.2/ex-1.16.scm
@@ -1,13 +1,28 @@
 ;;; Exercise 1.16
-(define (fast-expr b n)
- (define (expr-iter a br nr)
+
+(define (fast-expt b n)
+ (define (expt-iter a br nr)
    (cond ((= nr 0) a)
-	 ((even? nr) (expr-iter a (square br) (/ nr 2)))
-	 (else (expr-iter (* a br) br (- nr 1)))))
- (expr-iter 1 b n))
+	 ((even? nr) (expt-iter a (square br) (/ nr 2)))
+	 (else (expt-iter (* a br) br (- nr 1)))))
+ (expt-iter 1 b n))
 
 (define (even? n)
   (= (remainder n 2) 0))
 
 (define (square x)
   (* x x))
+
+;;; Examples
+
+;; (define (expt b n)
+;;   (if (= n 0)
+;;       1
+;;       (* b (expt b (- n 1)))))
+
+(define (expt b n)
+  (define (expt-iter counter product)
+    (if (= counter n)
+	product
+	(expt-iter (+ counter 1) (* b product))))
+  (expt-iter 0 1))
