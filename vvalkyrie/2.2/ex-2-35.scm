48b2bc3f525c1ee24a1dfed3e9b54e6a0a910e24
ikpark <ikpark@gmail.com>
2014-12-21 17:12:38 +0900

    ex 2.35 by vvalkyrie

---
+++ vvalkyrie/2.2/ex-2-35.scm
@@ -0,0 +1,18 @@
+; ex 2.35
+
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (accumulate op initial (cdr sequence)))))
+
+; fill <??>
+
+(define (horner-eval x coefficient-sequence)
+  (accumulate (lambda (this-coeff higher-terms)
+                (+ (* higher-terms x) this-coeff)) ;; <??>
+              0
+              coefficient-sequence))
+
+(horner-eval 2 (list 1 3 0 5 0 1))
+;;=> 79
