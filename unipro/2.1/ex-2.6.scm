1352185135b503bab3edff775371c0cc1ba8faa3
Daniel Jun <unipro.kr@gmail.com>
2015-01-06 18:06:44 +0900

    ex 2.6 updated by unipro

--- unipro/2.1/ex-2.6.scm
+++ unipro/2.1/ex-2.6.scm
@@ -1,74 +1,64 @@
-(define zero (lambda (f) (lambda (x) x)))
+;;; Church Numerals
+;;; Assume we have a programming language that doesn't support numbers
+;;; or booleans: a lambda is the only value it provides.
+;;; We can create some system that allows us to count, add, multiply,
+;;; and do all other things we do with numbers.
 
-(define (add-1 n)
+;;; Church numerals use lambdas to create a representation of
+;;; numbers. The idea is closely related to the functional
+;;; representation of natural numbers, i.e. to have a natural number
+;;; representing "zero" and a function "succ" that returns the
+;;; successor of the natural number it was given.
+
+;;; All Church numerals are functions with two parameters
+;;;  (lambda (f) (lambda (x) (something)))
+
+;;; The first parameter, f, is the successor function that should be
+;;; used.  The second parameter, x, is the value that represents zero.
+;;; Therefore, The Church numerals for zero is
+(define C0 (lambda (f) (lambda (x) x)))
+
+;;; The Church numerals for one applies the successor function to the
+;;; value representing zero exactly once:
+(define C1 (lambda (f) (lambda (x) (f x))))
+
+;;; The Church numerals for two, three, four, ..., n
+(define C2 (lambda (f) (lambda (x) (f (f x)))))
+(define C3 (lambda (f) (lambda (x) (f (f (f x))))))
+(define C4 (lambda (f) (lambda (x) (f (f (f (f x)))))))
+;; (define Cn (lambda (f) (lambda (x) (f (f (f (f (f (f x)))))))))
+;;                                    ~~~~~~~ n ~~~~~~~
+
+;; The addition function (plus C3 C4)
+;; (plus C3 C4)
+;; => (lambda (f) (lambda (x) ((C3 f) ((C4 f) x))))
+;; => (lambda (f) (lambda (x) (((lambda (f3) (lambda (x3) (f3 (f3 (f3 x3))))) f) ((C4 f) x))))
+;; => (lambda (f) (lambda (x) ((lambda (x3) (f (f (f x3)))) ((C4 f) x))))
+;; => (lambda (f) (lambda (x) ((lambda (x3) (f (f (f x3)))) (((lambda (f4) (lambda (x4) (f4 (f4 (f4 (f4 x4)))))) f) x))))
+;; => (lambda (f) (lambda (x) ((lambda (x3) (f (f (f x3)))) ((lambda (x4) (f (f (f (f x4))))) x))))
+;; => (lambda (f) (lambda (x) ((lambda (x3) (f (f (f x3)))) (f (f (f (f x)))))))
+;; => (lambda (f) (lambda (x) (f (f (f (f (f (f (f x)))))))))
+(define (plus m n)
+  (lambda (f) (lambda (x) ((m f) ((n f) x)))))
+
+;;; The successor function (succ n) is β-equivalent to (plus C1 n)
+;; (succ n)
+;; => (plus C1 n)
+;; => (lambda (f) (lambda (x) ((C1 f) ((n f) x))))
+;; => (lambda (f) (lambda (x) (((lambda (f1) (lambda (x1) (f1 x1))) f) ((n f) x))))
+;; => (lambda (f) (lambda (x) ((lambda (x1) (f x1)) ((n f) x))))
+;; => (lambda (f) (lambda (x) (f ((n f) x))))
+(define (succ n)
   (lambda (f) (lambda (x) (f ((n f) x)))))
 
-;; (add-1 zero)
-;; =>
-;; (add-1 (lambda (f) (lambda (x) x)))
-;; =>
-;; (lambda (f) (lambda (x) (f (((lambda (f') (lambda (x') x')) f) x))))
-;; =>
-;; (lambda (f) (lambda (x) (f ((lambda (x') x') x))))
-;; =>
-;; (lambda (f) (lambda (x) (f x)))
-(define one (lambda (f) (lambda (x) (f x))))
+;; (succ C0)
+;; => (succ (lambda (f) (lambda (x) x)))
+;; => (lambda (f) (lambda (x) (f (((lambda (f0) (lambda (x0) x0)) f) x))))
+;; => (lambda (f) (lambda (x) (f ((lambda (x0) x0) x))))
+;; => (lambda (f) (lambda (x) (f x)))
 
-;; (add-1 one)
-;; =>
-;; (add-1 (lambda (f) (lambda (x) (f x))))
-;; =>
-;; (lambda (f) (lambda (x) (f (((lambda (f') (lambda (x') (f' x'))) f) x))))
-;; =>
-;; (lambda (f) (lambda (x) (f ((lambda (x') (f x')) x))))
-;; =>
-;; (lambda (f) (lambda (x) (f (f x))))
-(define two (lambda (f) (lambda (x) (f (f x)))))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; in terms of repeated application of add-1
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; recursive version
-(define (+ n m)
-  (if (= m 0)
-      n
-      (add-1 (+ n (- m 1)))))
-
-;; iterative version
-(define (+ n m)
-  (define (iter result count)
-    (if (= count 0)
-	result
-	(iter (add-1 result) (- count 0))))
-  (iter n m))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; not in terms of repeated application of add-1
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-;; (+ two 3)
-;; =>
-;; (+ (lambda (f) (lambda (x) (f (f x)))) 3)
-;; =>
-;; ... (???)
-;; =>
-;; (lambda (f) (lambda (x) (f (f (f (f (f x)))))))
-;;
-;; (???)
-;; <=
-;; (lambda (f) (lambda (x) (f (f (f ((lambda (x') (f (f x'))) x))))))
-;; <=
-;; (lambda (f) (lambda (x) (f (f (f (((lambda (f') (lambda (x') (f' (f' x')))) f) x))))))
-;; <=
-;; (lambda (f) (lambda (x) (f (f (f (((lambda (f') (lambda (x') (f' (f' x')))) f) x))))))
-;; <=
-;; (lambda (f) (lambda (x) (f (f (f ((n f) x))))))
-
-(define (+ n m)
-  (lambda (f)
-    (lambda (x)
-      (define (iter result count)
-	(if (= count 0)
-	    result
-	    (iter (f result) (- count 1))))
-      (iter ((n f) x) m))))
+;; (succ C1)
+;; => (succ (lambda (f) (lambda (x) (f x))))
+;; => (lambda (f) (lambda (x) (f (((lambda (f1) (lambda (x1) (f1 x1))) f) x))))
+;; => (lambda (f) (lambda (x) (f ((lambda (x1) (f x1)) x))))
+;; => (lambda (f) (lambda (x) (f (f x))))
