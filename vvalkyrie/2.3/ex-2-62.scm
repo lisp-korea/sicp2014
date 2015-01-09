6b5197b1066baf498f078660a9d6685a71b6d233
ikpark <ikpark@gmail.com>
2015-01-09 13:43:58 +0900

    ex 2.61, 2.62 by vvalkyrie

---
+++ vvalkyrie/2.3/ex-2-62.scm
@@ -0,0 +1,20 @@
+;; ex 2.62
+
+(define (union-set set1 set2)
+  (cond ((null? set1) set2)
+        ((null? set2) set1)
+        (else
+          (let ((x1 (car set1)) (x2 (car set2)))
+            (cond ((= x1 x2)
+                   (cons x1 (union-set (cdr set1) (cdr set2))))
+                  ((< x1 x2)
+                   (cons x1 (union-set (cdr set1) set2)))
+                  ((> x1 x2)
+                   (cons x2 (union-set set1 (cdr set2)))))))))
+
+(union-set '(3 4 5) '(1 2 3))
+;;=> (1 2 3 4 5)
+
+(union-set '(3 4 5) '(1 2 3 4))
+;;=> (1 2 3 4 5)
+
