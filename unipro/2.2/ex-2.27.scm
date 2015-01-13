ee35b19f273543ee91676e17966c6288f75ae178
Daniel Jun <unipro.kr@gmail.com>
2015-01-13 10:48:02 +0900

    ex 2.27 by unipro

---
+++ unipro/2.2/ex-2.27.scm
@@ -0,0 +1,10 @@
+;;; Exercise 2.27
+
+(define (deep-reverse x)
+  (define (leaf? y) (not (pair? x)))
+
+  (if (leaf? x)
+      x
+      (let ((rx (reverse x)))
+	(cons (deep-reverse (car rx))
+	      (deep-reverse (cdr rx))))))
