d7d5ee48801baf617692eab9cd5b8ce0da1599c9
ikpark <ikpark@gmail.com>
2015-01-16 15:38:43 +0900

    ex 2.67~70 merged into a file (by vvalkyrie)

--- vvalkyrie/2.3/ex-2-67-70.scm
+++ vvalkyrie/2.3/ex-2-67-70.scm
@@ -1,4 +1,4 @@
-; ex 2.68
+; ex 2.67 - 2.70
 
 ; expressions
 
@@ -67,7 +67,7 @@
                              (cadr pair))
                   (make-leaf-set (cdr pairs))))))
 
-; exercise 2.67
+; ex 2.67
 
 (define sample-tree
   (make-code-tree (make-leaf 'A 4)
@@ -82,7 +82,7 @@
 (decode sample-message sample-tree)
 ;;=> (A D A B B C A)
 
-; exercise 2.68
+; ex 2.68
 
 (define (element-of-set? x set)
   (cond ((null? set) #f)
@@ -113,3 +113,47 @@
 
 (encode '(A D A B B C A) sample-tree)
 ;;=> (0 1 1 0 0 1 0 1 0 1 1 1 0)
+
+; ex 2.69
+
+(define (successive-merge set)
+  (if (null? (cdr set))
+    (car set)
+    (successive-merge
+      (adjoin-set ;; <!!>
+        (make-code-tree (car set) (cadr set))
+        (cdr (cdr set))))))
+
+(define (generate-huffman-tree pairs)
+  (successive-merge (make-leaf-set pairs)))
+
+(successive-merge (make-leaf-set '((D 1) (C 1) (A 4) (B 2))))
+;;=> ((leaf A 4) ((leaf B 2) ((leaf C 1) (leaf D 1) (D C) 2) (B D C) 4) (A B D C) 8)
+
+; ex 2.70
+
+(define rock-words
+  '((a 2) (boom 1) (get 2) (job 2) (na 16) (sha 3) (yip 9) (wah 1)))
+
+(define rock-words-tree
+  (generate-huffman-tree rock-words))
+;;=> ((leaf na 16) ((leaf yip 9) (((leaf a 2) ((leaf wah 1) (leaf boom 1) (wah boom) 2) (a wah boom) 4) ((leaf sha 3) ((leaf job 2) (leaf get 2) (job get) 4) (sha job get) 7) (a wah boom sha job get) 11) (yip a wah boom sha job get) 20) (na yip a wah boom sha job get) 36) 
+
+(define rock-lyrics
+  '(get a job
+    sha na na na na na na na na
+    get a job
+    sha na na na na na na na na
+    wah yip yip yip yip yip yip yip yip yip
+    sha boom))
+
+(encode rock-lyrics rock-words-tree)
+;;=> (1 1 1 1 1 1 1 0 0 1 1 1 1 0 1 1 1 0 0 0 0 0 0 0 0 0 1 1 1 1 1 1 1 0 0 1 1 1 1 0 1 1 1 0 0 0 0 0 0 0 0 0 1 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 1 1 0 1 1 0 1 1)
+;; (84 bits)
+
+;; in the case of encoding with fixed-length code,
+;; each word requires 3-bit.
+;; rock-lyrics has 36 words, so 3 x 36 = 108 bits required minimally.
+
+(decode (encode rock-lyrics rock-words-tree) rock-words-tree)
+;;=> (get a job sha na na na na na na na na get a job sha na na na na na na na na wah yip yip yip yip yip yip yip yip yip sha boom)
